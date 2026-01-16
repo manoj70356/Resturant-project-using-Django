2513be14d7e5b6381ed2605600b8f1f8fca91bc1
manoj70356 <manojsingh805730@gmail.com>
2026-01-16 23:26:23 +0530

    html part is completed

--- base_app/urls.py
+++ base_app/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('menu', views.menu, name='menu'),
+    path('book-table', views.book, name='book'),
 ]
+
